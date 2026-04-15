198da3adef4007e44db508fe329437be06aa0247
Ravihattarki <rhme073@gmail.com>
2026-04-15 22:20:06 +0530

    first commit

---
+++ helloworld.java
@@ -0,0 +1,10 @@
+public class helloworld {
+	public static void main(String[] args) {
+		System.out.println("Fun Facts about 26 January:\n");
+		System.out.println("1. In India, 26 January is celebrated as Republic Day, marking the date in 1950 when the Constitution of India came into effect.");
+		System.out.println("2. The day is also known as National Holiday in India, with a grand parade in New Delhi featuring cultural performances and military displays.");
+		System.out.println("3. In 1930, the Indian National Congress declared 26 January as Purna Swaraj Day, or Complete Independence Day.");
+		System.out.println("4. Outside India, 26 January is also Australia Day, the official national day of Australia.");
+		System.out.println("5. This date is a reminder of democracy and national unity in multiple countries around the world.");
+	}
+}
